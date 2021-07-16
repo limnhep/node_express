1622952a55452891b1e6851f0d144e1da2022220
Lim Nhep <limenhep@gmail.com>
2021-07-15 18:32:53 -0700

    Add a post request and add nodemon dependency

--- app.js
+++ app.js
@@ -1,6 +1,59 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
+
+// --- MIDDLEWARE ---
+
+app.use(express.json());
+
+// --- ENDPOINTS ---
+
+// --- GET TOURS FILE ---
+
+const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+const toursJSON = JSON.parse(tours);
+
+// --- READ TOURS ---
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'Success',
+      results: toursJSON.length,
+      data: {
+        tours: toursJSON,
+      },
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = toursJSON[toursJSON.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  toursJSON.push(newTour);
+
+  fs.writeFile(`
+    ${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursJSON),
+    (err) => {
+      res
+        .status(201)
+        .json({
+          status: 'Success',
+          data: {
+            tour: newTour
+          }
+        })
+    })
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
+});
 
 // --- NO LONGER IN USE ---
 
@@ -16,24 +69,3 @@ const app = express();
 
   // app.get(string, a route handler -> a callback);
 */
-
-// --- ENDPOINTS ---
-
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
-const toursJSON = JSON.parse(tours);
-
-app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 'success',
-      data: {
-        tours: toursJSON,
-      }
-    })
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Listening on Port: ${port}`);
-})
