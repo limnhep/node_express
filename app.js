0f440bc89feff345cf037f8cdae19283b59c452b
Lim Nhep <limenhep@gmail.com>
2021-07-15 17:31:50 -0700

    Add get tours as an endpoint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -16,9 +17,21 @@ const app = express();
   // app.get(string, a route handler -> a callback);
 */
 
-app.get('/api/v1/tours', (req, res) => {
+// --- ENDPOINTS ---
 
-})
+const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+const toursJSON = JSON.parse(tours);
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'success',
+      data: {
+        tours: toursJSON,
+      }
+    })
+});
 
 const port = 3000;
 app.listen(port, () => {
